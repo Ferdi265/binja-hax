ec4a046c00fb80c3ba9eb243eb08c59f754b77c5
Ferdinand Bachmann <theferdi265@gmail.com>
2020-07-14 19:41:22 +0200

    symver_hack: nicer symbol order

--- symver_hack/symver_hack.s
+++ symver_hack/symver_hack.s
@@ -9,8 +9,8 @@ __fix_\sym:
 .symver __fix_\sym, \sym@\ver
 .endm
 
-fix_symver _ZSt24__throw_out_of_range_fmtPKcz Qt_5
 fix_symver _ZdaPvm Qt_5
 fix_symver _ZdlPvm Qt_5
+fix_symver _ZSt24__throw_out_of_range_fmtPKcz Qt_5
 fix_symver __cxa_throw_bad_array_new_length Qt_5
 fix_symver _ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE9_M_createERmm Qt_5
